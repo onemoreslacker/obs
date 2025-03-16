d08a25b20a078860fb20277309453eca8d31f2ca
shadywarder <shadywarder@gmail.com>
2025-03-16 23:34:49 +0300

    feat: implement scrapper server

---
+++ internal/infrastructure/servers/scrapper_server/server.go
@@ -0,0 +1,21 @@
+package scrapperserver
+
+import (
+	"net"
+	"net/http"
+	"time"
+
+	scrapperapi "github.com/es-debug/backend-academy-2024-go-template/api/openapi/v1/scrapper_api"
+	"github.com/es-debug/backend-academy-2024-go-template/internal/config"
+)
+
+// New instantiates a new http.Server entity.
+func New(cfg *config.Config, api *scrapperapi.API) *http.Server {
+	mux := http.NewServeMux()
+
+	return &http.Server{
+		Addr:              net.JoinHostPort(cfg.Host, cfg.ScrapperPort),
+		Handler:           scrapperapi.HandlerFromMux(api, mux),
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+}
